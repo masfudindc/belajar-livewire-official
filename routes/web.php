29f7bca672bfca28e0965d7804247a5dd97f65c7
masfudindc <masfudindc@gmail.com>
2026-01-12 22:35:16 +0700

    feat: add layout to render page with $slot

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,24 @@
 <?php
 
+use App\Livewire\Counter;
+use App\Livewire\Todos;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/hello', function () {
-    return view('livewire.hello-world');
-});
+// Route::get('/hello', function () {
+//     return view('livewire.hello-world');
+// });
 
-Route::get('/todos', function () {
-    return view('livewire.hello-world');
-});
+// Route::get('/todos', function () {
+//     return view('livewire.hello-world');
+// });
+
+// Route::get('/counter', function () {
+//     return view('livewire.counter');
+// });
+
+Route::get('/', Todos::class);
+Route::get('/counter', Counter::class);
